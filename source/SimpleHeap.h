0f160e3a1d37b8b7b6a22c5587d09e9d713fb805
HotKeyIt <bios2000@web.de>
2010-02-03 23:23:41 +0100

    Version H3 - Unicode

--- source/SimpleHeap.h
+++ source/SimpleHeap.h
@@ -17,8 +17,6 @@ GNU General Public License for more details.
 #ifndef SimpleHeap_h
 #define SimpleHeap_h
 
-#include "stdafx.h" // pre-compiled headers
-
 // In a large script (200 KB of text) of a typical nature, using SimpleHeap rather than malloc() saves
 // nearly 200 KB of memory as shown by Task Manager's "VM Size" column (2384 vs. 2580 KB).
 // This is because many callers allocate chunks of memory that are very small on average.  If each
@@ -35,18 +33,18 @@ GNU General Public License for more details.
 // (the maximum line length that can be loaded -- currently 16K), otherwise,
 // memory for that line might be impossible to allocate.
 // Update: reduced it from 64K to 32K since many scripts tend to be small.
-#define BLOCK_SIZE (32 * 1024) // Relied upon by Malloc() to be a multiple of 4.
+// Update 2 (fincs): Use twice as big blocks when compiling for Unicode because
+// Unicode strings are twice as large.
+#define BLOCK_SIZE (32 * 1024 * sizeof(TCHAR)) // Relied upon by Malloc() to be a multiple of 4.
 
 class SimpleHeap
 {
 private:
 
-	static SimpleHeap *CreateBlock();
 	SimpleHeap();  // Private constructor, since we want only the static methods to be able to create new objects.
-	
 public:
-//	static UINT GetBlockCount() {return sBlockCount;}
 	~SimpleHeap();
+	static SimpleHeap *CreateBlock();
 	char *mBlock; // This object's memory block.  Although private, its contents are public.
 	char *mFreeMarker;  // Address inside the above block of the first unused byte.
 	size_t mSpaceAvailable;
@@ -54,9 +52,9 @@ public:
 	static SimpleHeap *sFirst, *sLast;  // The first and last objects in the linked list.
 	static char *sMostRecentlyAllocated; // For use with Delete().
 	SimpleHeap *mNextBlock;  // The object after this one in the linked list; NULL if none.
-// Naveen made above public to free memory on late included scripts
-	static char *Malloc(char *aBuf, size_t aLength = -1); // Return a block of memory to the caller and copy aBuf into it.
-	static char *Malloc(size_t aSize); // Return a block of memory to the caller.
+//	static UINT GetBlockCount() {return sBlockCount;}
+	static LPTSTR Malloc(LPTSTR aBuf, size_t aLength = -1); // Return a block of memory to the caller and copy aBuf into it.
+	static void* Malloc(size_t aSize); // Return a block of memory to the caller.
 	static void Delete(void *aPtr);
 	//static void DeleteAll();
 };
