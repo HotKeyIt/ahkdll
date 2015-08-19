0b819c0b08d934974948f186f13970818090b032
HotKeyIt <bios2000@web.de>
2015-08-19 08:16:51 +0200

    Merge 1.1.22.04

--- source/script_object_bif.cpp
+++ source/script_object_bif.cpp
@@ -837,4 +837,7 @@ BIF_DECL(BIF_ObjRawSet)
 	}
 	if (!obj->SetItem(*aParam[1], *aParam[2]))
 		aResult = g_script.ScriptError(ERR_OUTOFMEM);
+	
+	aResultToken.symbol = SYM_STRING;
+	aResultToken.marker = _T("");
 }
