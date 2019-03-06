9c54984b3fa7dcc2ef568c31a24c8821eaea6dc4
HotKeyIt <bios2000@web.de>
2019-03-06 02:18:17 +0100

    Fix sStdModule

--- source/script2.cpp
+++ source/script2.cpp
@@ -11335,6 +11335,12 @@ void *GetDllProcAddress(LPCTSTR aDllFileFunc, HMODULE *hmodule_to_free) // L31: 
 	char *function_name;
 #endif
 
+	// Define the standard libraries here. If they reside in %SYSTEMROOT%\system32 it is not
+	// necessary to specify the full path (it wouldn't make sense anyway).
+	static HMODULE sStdModule[] = {GetModuleHandle(_T("user32")), GetModuleHandle(_T("kernel32"))
+		, GetModuleHandle(_T("comctl32")), GetModuleHandle(_T("gdi32"))}; // user32 is listed first for performance.
+	static const int sStdModule_count = _countof(sStdModule);
+
 	// Make a modifiable copy of param1 so that the DLL name and function name can be parsed out easily, and so that "A" or "W" can be appended if necessary (e.g. MessageBoxA):
 	tcslcpy(param1_buf, aDllFileFunc, _countof(param1_buf) - 1); // -1 to reserve space for the "A" or "W" suffix later below.
 	if (   !(_tfunction_name = _tcsrchr(param1_buf, '\\'))   ) // No DLL name specified, so a search among standard defaults will be done.
@@ -11348,8 +11354,8 @@ void *GetDllProcAddress(LPCTSTR aDllFileFunc, HMODULE *hmodule_to_free) // L31: 
 #endif
 
 		// Since no DLL was specified, search for the specified function among the standard modules.
-		for (i = 0; i < g_StdModule_count; ++i)
-			if (   g_StdModule[i] && (function = (void *)GetProcAddress(g_StdModule[i], function_name))   )
+		for (i = 0; i < sStdModule_count; ++i)
+			if (   sStdModule[i] && (function = (void *)GetProcAddress(sStdModule[i], function_name))   )
 				break;
 		if (!function)
 		{
@@ -11358,8 +11364,8 @@ void *GetDllProcAddress(LPCTSTR aDllFileFunc, HMODULE *hmodule_to_free) // L31: 
 			// function) seems unacceptably high in a custom DLL.  For example, a custom DLL might have
 			// function called "AA" but not one called "A".
 			strcat(function_name, WINAPI_SUFFIX); // 1 byte of memory was already reserved above for the 'A'.
-			for (i = 0; i < g_StdModule_count; ++i)
-				if (   g_StdModule[i] && (function = (void *)GetProcAddress(g_StdModule[i], function_name))   )
+			for (i = 0; i < sStdModule_count; ++i)
+				if (   sStdModule[i] && (function = (void *)GetProcAddress(sStdModule[i], function_name))   )
 					break;
 		}
 	}
@@ -11391,8 +11397,8 @@ void *GetDllProcAddress(LPCTSTR aDllFileFunc, HMODULE *hmodule_to_free) // L31: 
 			// v1.0.34: If it's one of the standard libraries, try the "A" suffix.
 			// jackieku: Try it anyway, there are many other DLLs that use this naming scheme, and it doesn't seem expensive.
 			// If an user really cares he or she can always work around it by editing the script.
-			//for (i = 0; i < g_StdModule_count; ++i)
-			//	if (hmodule == g_StdModule[i]) // Match found.
+			//for (i = 0; i < sStdModule_count; ++i)
+			//	if (hmodule == sStdModule[i]) // Match found.
 			//	{
 					strcat(function_name, WINAPI_SUFFIX); // 1 byte of memory was already reserved above for the 'A'.
 					function = (void *)GetProcAddress(hmodule, function_name);
